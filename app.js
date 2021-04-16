02b2c4e74dd0cb113ae1e0734843933d2ba5f0d8
Pepe Valdivia <jvaldivia@softweb.pe>
2021-04-16 14:38:38 -0500

    error post 404

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // load controllers
 boostrap(app);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(middlewares.error404());
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,3 +1,5 @@
+const { getContent } = require('./contents');
+
 function preResponse(){
   return function (req, res, next) {
     res.set('Server', 'Ubuntu');
@@ -5,4 +7,23 @@ function preResponse(){
   }
 }
 
+function error404(){
+  return function (req, res, next) {
+    var lang = 'sp';
+    if ('GET' == req.method){
+      var resource = req.path.split('.');
+      var extensions = ['css', 'js', 'png', 'jpg', ];
+      if(extensions.indexOf(resource[resource.length - 1]) == -1){
+        res.redirect('/error/access/404');
+      }else {
+        return next();
+      }
+    }else{
+      var resp = getContent('error')[lang]['error_handler']['post_404'];
+      res.status(404).send(resp);
+    }
+  }
+}
+
 exports.preResponse= preResponse;
+exports.error404 = error404;
