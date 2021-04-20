fe1661390a37fcb53526d3df1f901b3f09062c7a
Pepe Valdivia <jvaldivia@softweb.pe>
2021-04-20 12:46:52 -0500

    agregar carrera

--- api/controllers/carrer_controller.js
+++ api/controllers/carrer_controller.js
@@ -22,4 +22,56 @@ router.get('', async (req, res, next) => {
   res.status(200).render('carrer/index', locals);
 });
 
+router.get('/add', (req, res, next) => {
+  // logic
+  var formTitle = 'Agregar Carrera';
+  // response
+  var locals = {
+    constants: constants,
+    title: 'Bienvenido',
+    helpers: helpers,
+    csss: indexCss(),
+    session: req.session,
+    formTitle: formTitle,
+    action: 'add',
+    jss: indexJs(),
+    contents: {},
+  };
+  res.status(200).render('carrer/detail', locals);
+});
+
+router.post('/add', async (req, res, next) => {
+  // data
+  var name = req.body.name;
+  // logic
+  try{
+    await Carrer.create({
+      name: name,
+    }); 
+  }catch(error){
+    console.log(error);
+    return res.redirect('/carrer?prev=add-error');
+  } 
+  // response
+  return res.redirect('/carrer?prev=add-ok');
+});
+
+router.get('/edit/:id', async (req, res, next) => {
+  // logic
+  var formTitle = 'Editar Carrera';
+  // response
+  var locals = {
+    constants: constants,
+    title: 'Bienvenido',
+    helpers: helpers,
+    csss: indexCss(),
+    session: req.session,
+    formTitle: formTitle,
+    action: 'edit',
+    jss: indexJs(),
+    contents: {},
+  };
+  res.status(200).render('carrer/detail', locals);
+});
+
 module.exports = router; 
