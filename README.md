d6adccce037b468eea5983bf1c590505c92e3264
Pepe Valdivia <jvaldivia@softweb.pe>
2021-05-14 20:37:55 -0500

    websocket con bin/www

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ Fuentes:
 + https://expressjs.com/es/starter/generator.html
 + https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 + https://getbootstrap.com/docs/4.0/examples/carousel/
++ https://libraries.io/bower/socket.io-client
++ https://stackoverflow.com/questions/24609991/using-socket-io-in-express-4-and-express-generators-bin-www
---
+++ config/sockets.js
@@ -0,0 +1,19 @@
+var socketIO = require('socket.io');
+var io = socketIO();
+var socketApi = {};
+
+socketApi.io = io;
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+ });
+});
+
+socketApi.sendNotification = function() {
+  io.sockets.emit('hello', {msg: 'Hello World!'});
+}
+
+module.exports = socketApi;
