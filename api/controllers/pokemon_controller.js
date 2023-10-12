4f17f0a26bba4b83a705bfb14c7ce44bd5b69e81
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-10-11 23:18:57 -0500

    listado de pokemones

--- api/controllers/pokemon_controller.js
+++ api/controllers/pokemon_controller.js
@@ -3,9 +3,18 @@ var router = express.Router();
 const Pokemon = require('../models/pokemon');
 
 router.get('/list', async (req, res, next) => {
-  var list = await Pokemon.findAll({
-    limit: 20
-  });
+  // data
+  let generationId = (typeof req.query.name === 'undefined') ? 0 : req.query.generation_id ;
+  // logic
+  let query = {}
+  if (generationId != 0){
+    query.where = {
+      generationId: generationId
+    }
+  }else{
+    query.limit = 30
+  }
+  var list = await Pokemon.findAll(query);
   return res.send(JSON.stringify(list)).status(200);
 });
 
