f2b38066f8f4cbfc859b92fa06108f312a55eeff
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-10-23 17:03:32 -0500

    listar partes del cuerpo

--- api/controllers/body_part_controller.js
+++ api/controllers/body_part_controller.js
@@ -1,4 +1,19 @@
 var express = require('express');
+const BodyPart = require('../models/body_part');
 var router = express.Router();
+
+router.get('/list', (req, res, next) => {
+  BodyPart.findAll()
+    .then(list => {
+      if (list) {
+        res.send(JSON.stringify(list)).status(200);
+      } else {
+        res.send('Lista no encontrada').status(404);
+      }
+    })
+    .catch(err => {
+      console.error('Error al seleccionar los registros:', err);
+    });
+});
 
 module.exports = router;
